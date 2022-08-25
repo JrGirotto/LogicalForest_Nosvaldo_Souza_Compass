4f63d07a8d3563754d6e14aafb0ed5b91e7a0bc1
jrgirotto <jrgirotto@live.com>
2022-08-25 13:07:23 -0300

    miniProjeto02_masDoisValores

--- Curso de Javascript/aula.js
+++ Curso de Javascript/aula.js
@@ -1,1 +1,10 @@
-console.log('junior')
+//console.log('junior')
+
+const pessoa = {
+	nome: "Junior",
+	idade: 41
+};
+
+for (let chave in pessoa) {
+	console.log(chave, pessoa['nome', 'idade']);
+}
